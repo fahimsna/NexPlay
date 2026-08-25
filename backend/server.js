88997740ddd60f922506ef8eaaa8ff232fd049a3
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-25 10:30:14 +0600

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,13 @@ const app = express();
 // =======================
 // DATABASE CONNECTION
 // =======================
+
 connectDB();
 
 // =======================
 // MIDDLEWARE
 // =======================
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -33,12 +35,13 @@ app.use(express.urlencoded({ extended: true }));
 // =======================
 // STATIC FILES
 // =======================
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/api/activity", activityRoutes);
 
 // =======================
 // ROUTES
 // =======================
+
 const authRoutes = require("./routes/authRoutes");
 const companyRoutes = require("./routes/companyRoutes");
 const advertisementRoutes = require("./routes/advertisementRoutes");
@@ -48,23 +51,42 @@ const analyticsRoutes = require("./routes/analyticsRoutes");
 const upcomingRoutes = require("./routes/upcomingRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
+
 // =======================
 // API ROUTES
 // =======================
+
 app.use("/api/auth", authRoutes);
+
 app.use("/api/company", companyRoutes);
+
 app.use("/api/advertisements", advertisementRoutes);
+
 app.use("/api/campaigns", campaignRoutes);
+
 app.use("/api/dashboard", dashboardRoutes);
+
 app.use("/api/analytics", analyticsRoutes);
+
 app.use("/api/upcoming", upcomingRoutes);
+
 app.use("/api/settings", settingsRoutes);
+
 app.use("/api/sports", sportsRoutes);
+
 app.use("/api/admin", adminRoutes);
+
 app.use("/api/reviews", reviewRoutes);
+
+// Sprint 4
+// User Activity History
+
+app.use("/api/activity", activityRoutes);
+
 // =======================
 // ROOT ROUTE
 // =======================
+
 app.get("/", (req, res) => {
   res.send("NexPlay Backend Running");
 });
@@ -72,6 +94,7 @@ app.get("/", (req, res) => {
 // =======================
 // SERVER
 // =======================
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
