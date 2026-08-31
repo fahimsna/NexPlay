c0806e91f71bf14be59c2455ab6b3390defa3f31
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-31 16:19:09 +0600

    Fix production authentication API

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,33 +1,25 @@
-import axios from "../api/axiosInstance";
+import API from "./api";
 
 // REGISTER
-
 export const registerService = async (data) => {
-  const response = await axios.post("/auth/register", data);
-
+  const response = await API.post("/auth/register", data);
   return response.data;
 };
 
 // LOGIN
-
 export const loginService = async (data) => {
-  const response = await axios.post("/auth/login", data);
-
+  const response = await API.post("/auth/login", data);
   return response.data;
 };
 
 // GET CURRENT USER
-
 export const getMeService = async () => {
-  const response = await axios.get("/auth/me");
-
+  const response = await API.get("/auth/me");
   return response.data;
 };
 
 // LOGOUT
-
 export const logoutService = () => {
   localStorage.removeItem("token");
-
   localStorage.removeItem("user");
 };
